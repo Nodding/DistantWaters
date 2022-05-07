d623b21ae5f03787ebdc5fa7ed1243d3ea65df9d
John Cinquegrana <alllegron@gmail.com>
2022-05-07 13:13:01 -0400

    Re-added primary protocals to main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+// import { appendChild } from "parse5/lib/tree-adapters/default";
 import { createApp } from "vue";
 import App from "./App.vue";
+import { createPinia } from "pinia";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 
+//  Create the app we are going to use
+const app = createApp(App);
+
+//  Load fonts for Vuetify
 loadFonts();
 
-createApp(App).use(vuetify).mount("#app");
+//  Load in Pinia
+app.use(createPinia());
+
+//  Allow the app to use everything vuetify
+app.use(vuetify);
+
+//  Attach the Vue app to the html page
+app.mount("#app");
