66349756e5e1473f0934d958b012f2224925dab9
John Cinquegrana <alllegron@gmail.com>
2022-05-08 19:09:15 -0400

    Home page overhauling

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,13 +1,32 @@
 <script setup lang="ts"></script>
 
 <template>
-  <v-main>
+  <v-main class="d-flex flex-column align-stretch">
     <v-container class="d-flex flex-column justify-center align-center h-100">
       <div class="text-center text-h1 pa-5">Distant Waters</div>
       <div class="text-center text-h3 text-grey-darken-2 pa-5">
         A Game of Gold and Lighthouses
       </div>
     </v-container>
+    <v-img src="./src/assets/layered-waves.svg" />
+    <v-img src="./src/assets/reverse-waves.svg" />
+    <div class="pa-0 ma-0 w-100" style="background-color: #383e6d">
+      <v-sheet
+        class="d-flex flex-row justify-space-around"
+        style="background-color: #383e6d"
+      >
+        <v-card class="ma-5 pa-5 bg-blue-darken-4">
+          <v-card-title>This is how you play the game</v-card-title>
+          <v-card-subtitle>Oooh how exciting</v-card-subtitle>
+          <v-card-text>Please style me better</v-card-text>
+        </v-card>
+        <v-card class="ma-5 pa-5 bg-blue-darken-4">
+          <v-card-title>More stuff about playing the game</v-card-title>
+          <v-card-subtitle>OMG I really want to play it</v-card-subtitle>
+          <v-card-text>Please style me better</v-card-text>
+        </v-card>
+      </v-sheet>
+    </div>
   </v-main>
 </template>
 
