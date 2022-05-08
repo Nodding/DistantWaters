b5c841ac0fd5969dd26bbe43ad50e95a52b25adf
John Cinquegrana <alllegron@gmail.com>
2022-05-08 17:55:54 -0400

    Updated Main Page with title

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -2,9 +2,12 @@
 
 <template>
   <v-main>
-    <div>
-      <P>This is the MainPage</P>
-    </div>
+    <v-container class="d-flex flex-column justify-center align-center h-100">
+      <div class="text-center text-h1 pa-5">Distant Waters</div>
+      <div class="text-center text-h3 text-grey-darken-2 pa-5">
+        A Game of Gold and Lighthouses
+      </div>
+    </v-container>
   </v-main>
 </template>
 
