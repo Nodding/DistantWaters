fac319592b8180daaf3603963b8a2da38c941606
John Cinquegrana <alllegron@gmail.com>
2022-05-24 21:20:29 -0400

    Added proper lighting to the scene and a backdrop color

--- src/scripts/createGame.ts
+++ src/scripts/createGame.ts
@@ -9,8 +9,9 @@
 */
 
 import * as three from "three";
+import { loadGLTFObject } from "./loadAssets";
 
-function createGame(id: string) {
+async function createGame(id: string) {
   //  Grab the container that will contain the game
   const gameContainer = document.getElementById(id);
   if (gameContainer == null) {
@@ -22,6 +23,9 @@ function createGame(id: string) {
   //  Configure the settings for the game
   const scene = new three.Scene();
 
+  //  Set scene background to a sky blue
+  scene.background = new three.Color(0x83d8e6);
+
   //camera settings (changing camera width is a great way to move the camera around)
   const aspectRatio = window.innerWidth / window.innerHeight;
   const cameraWidth = 25;
@@ -29,29 +33,43 @@ function createGame(id: string) {
 
   //    Set the size of the game
   const renderer = new three.WebGLRenderer();
-  renderer.setSize(window.innerWidth * 0.6, window.innerHeight * 0.5);
+  renderer.setSize(window.innerWidth - 200, window.innerHeight * 0.7);
   gameContainer.appendChild(renderer.domElement);
+
+  //  Configure Renderer for GLTF objects
+  renderer.outputEncoding = three.sRGBEncoding;
 
   //    Create the objects within the game
 
   //  Create the camera for the scene
+  const cameraScale = 0.1;
   const camera = new three.OrthographicCamera(
     //left
-    cameraWidth / -2,
+    cameraWidth / -cameraScale,
     //right
-    cameraWidth / 2,
+    cameraWidth / cameraScale,
     //top
-    cameraHeight / 2,
+    cameraHeight / cameraScale,
     //bottom
-    cameraHeight / -2
+    cameraHeight / -cameraScale
   );
 
   //    Move the camera to the proper location
-  camera.position.z = -5;
-  camera.position.y = 5;
+  camera.position.set(100, 100, -100);
 
   //  Set the camer to look at the origin
   camera.lookAt(0, 0, 0);
+
+  //  Configure the light for the scene
+
+  //  Create a soft ambient light so that everything can be seen
+  const ambLight = new three.AmbientLight(0x222222, 0.1);
+  scene.add(ambLight);
+
+  //  Create a softbox simulating sunlight that provides shadows to the scene
+  const sun = new three.DirectionalLight(0xffffb5, 0.7);
+  sun.position.set(100, 100, 0);
+  scene.add(sun);
 
   //  Mouse initialization and listener
   const mouse = new three.Vector3(0, 0, 0);
@@ -63,6 +81,13 @@ function createGame(id: string) {
   const geometry = new three.BoxGeometry();
   const material = new three.MeshBasicMaterial({ color: 0x00ff00 });
   const cube = new three.Mesh(geometry, material);
+  cube.position.set(0, 0, 0);
+  scene.add(cube);
+
+  //  Create the boat
+  const boat = await loadGLTFObject("src/assets/models/pirateship.glb");
+  boat.position.set(0, 0, 0);
+  scene.add(boat);
 
   //    Define the function which starts the game
   function animate() {
