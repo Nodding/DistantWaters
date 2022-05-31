21a091bdf5a0595bb35ed178ee6bbde37242e8ce
John Cinquegrana <alllegron@gmail.com>
2022-05-31 19:59:42 -0400

    Renamed Component

---
+++ src/components/HorizontalSheet.vue
@@ -0,0 +1,17 @@
+<!--
+   File: HorizontalDouble.vue
+   Created Date: 05-13-2022
+   Last Modified: Fri May 13 2022
+   ---------------------
+   Authors: John Cinquegrana, Lucca Cioffi
+        Distant Waters
+-->
+<script setup lang="ts"></script>
+
+<template>
+  <v-sheet class="d-flex flex-row justify-space-around">
+    <slot />
+  </v-sheet>
+</template>
+
+<style></style>
