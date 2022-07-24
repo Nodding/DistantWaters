a732a67efc9badc50ed933ad0e984bffb484087e
John Cinquegrana <alllegron@gmail.com>
2022-07-24 16:23:12 -0400

    Removed scaling after figuring out how blender works

--- src/scripts/loadAssets.ts
+++ src/scripts/loadAssets.ts
@@ -10,9 +10,6 @@
 import * as three from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
-//  Offset from Blender scale to our local scale
-const scaleOffset: [number, number, number] = [0.2, 0.2, 0.2];
-
 /**
  * Given a path to a file containing a 3D model, returns the model as an Object3D.
  * This is the superclass of all 3D objects in three.js
@@ -26,7 +23,6 @@ async function loadGLTFObject(path: string): Promise<THREE.Object3D> {
   //    Wait for the result that we need in order to load in the objects
   return loader.loadAsync(path).then((result) => {
     //  After getting the result, return the scene inside of it
-    result.scene.scale.set(...scaleOffset);
     return result.scene;
   });
 }
